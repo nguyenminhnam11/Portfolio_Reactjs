2fbec9215f3dc8ba9266a9f4928428945b6de949
Nam Nguyen <nguyenminhnam1739@gmail.com>
2024-05-22 23:50:35 +0700

    update project

--- src/assets/data/language.js
+++ src/assets/data/language.js
@@ -3,13 +3,11 @@ const languages = [
       code: "vi",
       name: "VIE",
       country_code: "vn",
-      flag: "../../images/En_Icon.svg"
     },
     {
       code: "en",
       name: "ENG",
       country_code: "us",
-      flag: "../../images/En_Icon.svg"
     },
   ];
 
--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,19 +1,32 @@
-import ui_e_commerce from '../../images/E-commerce.png'
-const projects = [
+import vatvo from '../../images/vatvoshop.png'
+import clock from '../../images/clock.png'
+import todo from '../../images/todolist.png'
+
+const projects = [  
     {
-        name: "UI Ecommerce",
-        image: ui_e_commerce,
-        description: "Simple interface for an online clothing selling site. And compatible on all screens from computer to mobile",
-        tech: ["HTML", "CSS", "Javascript", "Responsive"],
-        url: "https://e-commerce-ui-responsive.vercel.app/"
+        name: "Vật Vờ Shop",
+        image: vatvo,
+        description: "The website selling VatVo brand shirts only includes front-end work, relatively complete with functions such as adding to cart, customizing quantity, product size, and total invoice...",
+        tech: ["React", "Redux", "Json-Server", "Css", "Axios"],
+        url: "https://vat-vo-shop.vercel.app/",
+        git: "https://github.com/nguyenminhnam11/VatVoShop"
     },
     {
-        name: "shopdunk",
-        image: ui_e_commerce,
-        description: "Simple interface for an online clothing selling site. And compatible on all screens from computer to mobile",
-        tech: ["HTML", "CSS", "Javascript"],
-        url: "https://e-commerce-ui-responsive.vercel.app/"
-    }
+        name: "Todo App",
+        image: todo,
+        description: "Web-based application, is an application used to take notes and manage tasks to be done every day, with full functions of adding, deleting, editing tasks and storing on local storage.",
+        tech: ["React", "CSS", "Local Storage"],
+        url: "https://todo-list-mu-weld.vercel.app/",
+        git: "https://github.com/nguyenminhnam11/Todo-List"
+    },
+    {
+        name: "Clock App (Developing)",
+        image: clock,
+        description: "Is a clock application that includes features such as timer, alarm, stopwatch, countdown. In addition, the interface is inspired by the iPhone 15 Pro Max, with the ability to view battery percentage and charging status based on the computer's battery.",
+        tech: ["React", "CSS", "React-use", "Responsive"],
+        url: "https://clock-app-nine-sigma.vercel.app/",
+        git: "https://github.com/nguyenminhnam11/Clock-App"
+    },
 ]
 
 export default projects
