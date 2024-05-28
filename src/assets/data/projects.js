a9d2ce34c5902dad2e496d4f9dc2656345c3ef44
Nam Nguyen <nguyenminhnam1739@gmail.com>
2024-05-28 14:31:29 +0700

    add project spf

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,6 +1,7 @@
 import vatvo from '../../images/vatvoshop.png'
 import clock from '../../images/clock.png'
 import todo from '../../images/todolist.png'
+import shopeefood from '../../images/shopeefood.png'
 
 const projects = [  
     {
@@ -20,13 +21,21 @@ const projects = [
         git: "https://github.com/nguyenminhnam11/Todo-List"
     },
     {
+        name: "ShopeeFood - Delivery App",
+        image: shopeefood,
+        description: "This is an online food delivery website, it has a complete front end and responsive for mobile",
+        tech: ["React", "CSS", "React-router-dom", "Responsive"],
+        url: "https://shopee-food-clone.vercel.app/",
+        git: "https://github.com/nguyenminhnam11/ShopeeFood"
+    },
+    {
         name: "Clock App (Developing)",
         image: clock,
-        description: "Is a clock application that includes features such as timer, alarm, stopwatch, countdown. In addition, the interface is inspired by the iPhone 15 Pro Max, with the ability to view battery percentage and charging status based on the computer's battery.",
+        description: "Is a clock application that includes features. Also has the ability to view battery percentage and charge status based on the computer's battery.",
         tech: ["React", "CSS", "React-use", "Responsive"],
         url: "https://clock-app-nine-sigma.vercel.app/",
         git: "https://github.com/nguyenminhnam11/Clock-App"
-    },
+    }
 ]
 
 export default projects
